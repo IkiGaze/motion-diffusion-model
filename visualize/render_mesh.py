f1ff17e43fb964705d93d4167e0d8a75ef79cb27
felix-vargas <felix.vargas@sansano.usm.cl>
2023-06-20 17:42:22 -0400

    Changed visualize for pkl export

--- visualize/render_mesh.py
+++ visualize/render_mesh.py
@@ -16,18 +16,20 @@ if __name__ == '__main__':
     sample_i, rep_i = [int(e) for e in parsed_name.split('_')]
     npy_path = os.path.join(os.path.dirname(params.input_path), 'results.npy')
     out_npy_path = params.input_path.replace('.mp4', '_smpl_params.npy')
+    npy_path = "."+npy_path    
+    print(npy_path)
     assert os.path.exists(npy_path)
     results_dir = params.input_path.replace('.mp4', '_obj')
     if os.path.exists(results_dir):
         shutil.rmtree(results_dir)
-    os.makedirs(results_dir)
+    #os.makedirs(results_dir)
 
     npy2obj = vis_utils.npy2obj(npy_path, sample_i, rep_i,
                                 device=params.device, cuda=params.cuda)
 
     print('Saving obj files to [{}]'.format(os.path.abspath(results_dir)))
     for frame_i in tqdm(range(npy2obj.real_num_frames)):
-        npy2obj.save_obj(os.path.join(results_dir, 'frame{:03d}.obj'.format(frame_i)), frame_i)
+        npy2obj.save_obj('.'+os.path.join(results_dir, 'frame{:03d}.obj'.format(frame_i)), frame_i)
 
     print('Saving SMPL params to [{}]'.format(os.path.abspath(out_npy_path)))
-    npy2obj.save_npy(out_npy_path)
+    npy2obj.save_npy('.'+out_npy_path)
